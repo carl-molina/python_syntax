5148c20a5378341bc8c406a1201757147d8c965d
Teagan McClintock <teagan.mcclintock@gmail.com>
2023-11-26 18:13:02 -0500

    Finished Part 3

--- words.py
+++ words.py
@@ -1,5 +1,17 @@
-def print_upper_words(words):
+def print_upper_words(words, must_start_with = {"e"}):
     """Takes a list of words; prints each words on a separate line in
     uppercase"""
     for word in words:
-        print(word.upper())
+        word = word.upper()
+        for letter in must_start_with:
+            if word.startswith(letter.upper()):
+                print(word)
+
+
+word_list = ["hi", "who", "what", "emerge", "Event"]
+
+print_upper_words(word_list)
+
+print_upper_words(
+    ["hello", "hey", "goodbye", "yo", "yes"],
+    must_start_with={"h", "y"})
